613894afced0be73dbe0d7c8e8feafc7b0f26963
ysoni-dev <ysoni1615@gmail.com>
2024-02-24 01:17:29 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const translate = require('translate-google')
 const langdetect = require('langdetect')
+require('dotenv').config()
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
+app.use(cors())
+app.use(express.json())
 app.use(bodyParser.json());
 
 app.get('/', async function (req, res) {
